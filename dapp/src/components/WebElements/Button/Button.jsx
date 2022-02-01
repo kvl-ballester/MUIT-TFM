84c4f4bb1c53267c1722f0dee464c3ac1cc09cc5
kvl-ballester <kvl.ballester@gmail.com>
2022-02-01 23:06:42 +0100

    add prop to disable button

--- dapp/src/components/WebElements/Button/Button.jsx
+++ dapp/src/components/WebElements/Button/Button.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import "./button.scss"
 
-export const Button = ({content, onClickButton, blue, red}) => {
-  return <div className={`button ${blue ? 'blue' : ''} ${red? 'red' : ''}`}
-                onClick={() => onClickButton()}
+export const Button = ({content, onClickButton, blue, red, disabled}) => {
+  return <div className={`button ${blue ? 'blue' : ''} ${red? 'red' : ''} ${disabled? 'disabled' : ''} `}
+                onClick={() => {
+                  if (!disabled) {
+                    onClickButton()
+                  }
+                }}
     >
       {content}
   </div>;
