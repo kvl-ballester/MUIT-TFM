4fe75e5021cafbc12fc0183d6c5462e00ba39ce4
kvl-ballester <kvl.ballester@gmail.com>
2022-02-01 23:06:04 +0100

    highlight selected tab

--- dapp/src/components/WebElements/Navbar/Navbar.jsx
+++ dapp/src/components/WebElements/Navbar/Navbar.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React, {useState} from 'react'
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.scss"
 
+const rutas = [
+    {path:'/', tab:'Home'},
+    {path:'/exchange', tab:'Exchange'},
+    {path:'/tokens', tab:'Tokens'},
+]
+
 export const Navbar = () => {
+    const location = useLocation();
+    const [tabSelected, setTabSelected] = useState(() => {
+        return rutas.filter(obj => {
+            return obj.path === location.pathname
+        })[0].tab
+    });
+
+    
     return (
         <nav className="navbar">
             <ul className="container">
-                <Link className="item"  to="/"> Home </Link> 
-                <Link className="item"  to="/exchange">Exchange</Link>
-                <Link className="item" to="/tokens">Tokens</Link>
+                {rutas.map((ruta, index) => {
+                    return <Link key={index} className={`item ${tabSelected === ruta.tab ? 'selected' : ''}`} onClick={() => setTabSelected(ruta.tab)} to={ruta.path}> {ruta.tab} </Link> 
+
+                })}
             </ul>
         </nav>
     )
