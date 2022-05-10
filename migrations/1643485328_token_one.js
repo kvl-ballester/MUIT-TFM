bebaf36c6de3f5b87c695f953ce01f17625ab90c
kvl-ballester <kvl.ballester@gmail.com>
2022-05-10 22:01:29 +0200

    script to deploy token and create pool

--- migrations/1643485328_token_one.js
+++ migrations/1643485328_token_one.js
@@ -1,5 +1,6 @@
 const EXCHANGE_FACTORY = artifacts.require('ExchangeFactory')
 const TOKEN = artifacts.require('Token')
+const EXCHANGE = artifacts.require('Exchange')
 
 module.exports = async function(_deployer, network, accounts) {
   // Use deployer to state migration tasks.
@@ -7,6 +8,25 @@ module.exports = async function(_deployer, network, accounts) {
 
   await _deployer.deploy(TOKEN, 'Cardano', 'ADA', 10000, {from: accounts[1]})
   const ada = await TOKEN.deployed();
-
+  
   await factory.createExchange(ada.address)
+
+  // Exchange contract created
+  let exchangeAddres = await factory.token_to_exchange.call(ada.address)
+  const exchange = await EXCHANGE.at(exchangeAddres)
+
+  try {
+     // Approve 5000 tokens to exchange contract
+    await ada.approve(exchangeAddres, web3.utils.toBN('5000000000000000000000'), {from: accounts[1]})
+  } catch (err) {
+    console.log('Error in approve: ', err)
+  }
+ 
+  try {
+    // Add liquidity
+    await exchange.addLiquidity(web3.utils.toBN('5000000000000000000000'), {from: accounts[1], value: web3.utils.toWei(web3.utils.toBN('2'),"ether")})
+  } catch (err) {
+    console.log('Error en addLiquidity: ', err)
+  }
+  
 };
