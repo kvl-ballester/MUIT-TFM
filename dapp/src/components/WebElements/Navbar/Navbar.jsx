43e5a414fd551506e8829c43f714744eea4bcb28
kvl-ballester <kvl.ballester@gmail.com>
2022-05-10 21:59:54 +0200

    navbar component

--- dapp/src/components/WebElements/Navbar/Navbar.jsx
+++ dapp/src/components/WebElements/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
 import { Link, useLocation } from "react-router-dom";
+import { AiOutlineHome } from "react-icons/ai";
+import { RiExchangeFill } from "react-icons/ri";
+import { BiCoin } from "react-icons/bi";
 import "./Navbar.scss"
 
 const rutas = [
-    {path:'/', tab:'Home'},
-    {path:'/exchange', tab:'Exchange'},
-    {path:'/tokens', tab:'Tokens'},
+    {path:'/', icon: <AiOutlineHome />, tab:'Home'},
+    {path:'/exchange', icon: <RiExchangeFill />, tab:'Exchange'},
+    {path:'/tokens', icon: <BiCoin/>, tab:'Tokens'},
 ]
 
 export const Navbar = () => {
@@ -18,13 +21,35 @@ export const Navbar = () => {
 
     
     return (
-        <nav className="navbar">
-            <ul className="container">
-                {rutas.map((ruta, index) => {
-                    return <Link key={index} className={`item ${tabSelected === ruta.tab ? 'selected' : ''}`} onClick={() => setTabSelected(ruta.tab)} to={ruta.path}> {ruta.tab} </Link> 
+        <div className="navbar">
+            <nav >
+                <ul className="container">
+                    {rutas.map((ruta, index) => {
+                        return <Link key={index} className={`item ${tabSelected === ruta.tab ? 'selected' : ''}`} onClick={() => setTabSelected(ruta.tab)} to={ruta.path}> 
+                        {ruta.icon}  {ruta.tab} 
+                        </Link> 
 
-                })}
-            </ul>
-        </nav>
+                    })}
+                </ul>
+            </nav>
+            <div className="credits">
+                <div className="text">Powered by</div>
+                <div className="logos">
+                    <div className="tech">
+                        <a href="https://telefonicatech.com/" rel="noreferrer" target='_blank'>
+                            <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQGzlG2VfCLfSw/company-logo_200_200/0/1623757473246?e=1660176000&v=beta&t=Vw9t3EML2giVpEdlFmcX_frQDQNuKl41251DJKrIHQg" 
+                                height={35} alt='logo telefonica' />
+                        </a>
+                    </div>
+                    <div className="etsit">
+                        <a href="https://www.etsit.upm.es/" rel="noreferrer" target='_blank'>
+                            <img src="https://www.etsit.upm.es/fileadmin/template/images/LOGO_ETSIT_WT.gif" alt='logo etsit' height={25}/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
     )
 }
