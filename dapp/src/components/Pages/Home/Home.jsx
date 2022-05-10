f6f20e2f521dfbabf19f86e528cbda154a2cd3ec
kvl-ballester <kvl.ballester@gmail.com>
2022-05-10 21:52:37 +0200

    connect wallet to dapp

--- dapp/src/components/Pages/Home/Home.jsx
+++ dapp/src/components/Pages/Home/Home.jsx
@@ -1,12 +1,30 @@
+import { Button } from "../../WebElements/Button/Button";
+import React from 'react';
+
 import "./Home.scss"
 
-export function Home() {
+export function Home({currentAccount, setAccount}) {
+
+    const handleEnableEthreum = async () => {
+      if (currentAccount) return;
+      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      setAccount(accounts[0])
+
+    }
+
     return (
-      <>
         <main>
           <h2>Welcome to the homepage!</h2>
-          <p>You can do this, I believe in you.</p>
+          {!currentAccount && 
+            <Button blue content={"Enable ethereum"} onClickButton={handleEnableEthreum}/>
+          }
+          
+          {currentAccount &&
+            <div className="account-info">
+              Account selected: {currentAccount}
+            </div>
+          }
+
         </main>
-      </>
     );
   }
