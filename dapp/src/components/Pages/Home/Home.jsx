1d264e8195e86d5e571a03213c1efba5d2ce1e9e
kvl-ballester <kvl.ballester@gmail.com>
2022-06-17 10:49:02 +0200

    home page content

--- dapp/src/components/Pages/Home/Home.jsx
+++ dapp/src/components/Pages/Home/Home.jsx
@@ -1,30 +1,62 @@
 import { Button } from "../../WebElements/Button/Button";
 import React from 'react';
-
+import { } from "../../../";
 import "./Home.scss"
 
-export function Home({currentAccount, setAccount}) {
+export function Home({ currentAccount, setAccount }) {
 
-    const handleEnableEthreum = async () => {
-      if (currentAccount) return;
-      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      setAccount(accounts[0])
+  const handleEnableEthreum = async () => {
+    if (currentAccount) return;
+    let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounts[0])
 
-    }
-
-    return (
-        <main>
-          <h2>Welcome to the homepage!</h2>
-          {!currentAccount && 
-            <Button blue content={"Enable ethereum"} onClickButton={handleEnableEthreum}/>
-          }
-          
-          {currentAccount &&
-            <div className="account-info">
-              Account selected: {currentAccount}
-            </div>
-          }
-
-        </main>
-    );
   }
+
+  return (
+    <main>
+      <div className="home">
+        <h1>Decentralized Exchange Application</h1>
+        {!currentAccount &&
+          <Button blue content={"Connect "} onClickButton={handleEnableEthreum} />
+        }
+
+        {currentAccount &&
+          <div className="account-info">
+            Account selected: {currentAccount}
+          </div>
+        }
+
+        <div className="intructions">
+          <h3 className="wrapper">
+            In order to use the application you must:
+            <ul>
+              <li>Have installed <b>Metamask</b></li>
+              <li>Switch to the <b>Rinkeby Test Network</b></li>
+              <li>Lastly, connect account to the Blockchain</li>
+            </ul>
+          </h3>
+
+          <div className="wrapper">
+            <h2 className="title">Within this application you can:</h2>
+            <div className="option">
+              <h3>Create an <b>ERC-20 Token </b> and an <b>exchange pool</b></h3>
+              <div className="image-container">
+                <img src="https://i.imgur.com/oPnF3lp.gif" alt="create token steps" />
+              </div>
+            </div>
+
+            <div className="option">
+              <h3><b>Trade</b>  ERC-20 Tokens</h3>
+              <div className="image-container">
+                <img src="https://i.imgur.com/5K62cn9.gif" alt="trade token steps" />
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+    </main>
+  );
+}
