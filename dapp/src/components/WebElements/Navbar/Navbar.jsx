e3f8878888071ad055347aad58fc846e8800de4f
kvl-ballester <kvl.ballester@gmail.com>
2022-12-26 00:34:30 +0100

    remove footer logo

--- dapp/src/components/WebElements/Navbar/Navbar.jsx
+++ dapp/src/components/WebElements/Navbar/Navbar.jsx
@@ -35,12 +35,6 @@ export const Navbar = () => {
             <div className="credits">
                 <div className="text">Powered by</div>
                 <div className="logos">
-                    <div className="tech">
-                        <a href="https://telefonicatech.com/" rel="noreferrer" target='_blank'>
-                            <img src="https://media-exp1.licdn.com/dms/image/C4D0BAQGzlG2VfCLfSw/company-logo_200_200/0/1623757473246?e=1660176000&v=beta&t=Vw9t3EML2giVpEdlFmcX_frQDQNuKl41251DJKrIHQg" 
-                                height={35} alt='logo telefonica' />
-                        </a>
-                    </div>
                     <div className="etsit">
                         <a href="https://www.etsit.upm.es/" rel="noreferrer" target='_blank'>
                             <img src="https://www.etsit.upm.es/fileadmin/template/images/LOGO_ETSIT_WT.gif" alt='logo etsit' height={25}/>
